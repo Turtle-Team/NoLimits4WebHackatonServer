1222ea57978807463dae9e24e8b56d5d18b4080d
SagidKamilov <89615854074a@gmail.com>
2023-04-08 01:49:37 +0300

    новый тест для выявления ошибки

--- database/handler.py
+++ database/handler.py
@@ -7,18 +7,27 @@ class DataBase:
                                            password=setting.DATABASE['password'],
                                            port=5432)
         self.cursor = self.connection.cursor()
-    def login(self, email, password):
-        sql = '''SELECT * FROM users_ac WHERE email=%s AND password=%s;'''
-        self.cursor.execute(sql, (email, password))
-        return bool(self.cursor.fetchone())
 
+    def login(self, email, password):
+        try:
+            sql = '''SELECT * FROM users_ac WHERE email=%s AND password=%s;'''
+            self.cursor.execute(sql, (email, password))
+            return bool(self.cursor.fetchone())
+        except mysql.connector.Error as error:
+            print("Failed to execute query: {}".format(error))
 
     def register(self, name, email, password):
-        sql = '''INSERT INTO users_ac(id, name, email, password) VALUES (?, ?, ?, ?) RETURNING *'''
-        self.cursor.execute(sql, (name, email, password))
-        return self.connection.commit()
+        try:
+            sql = '''INSERT INTO users_ac(id, name, email, password) VALUES (?, ?, ?, ?) RETURNING *'''
+            self.cursor.execute(sql, (name, email, password))
+            return self.connection.commit()
+        except mysql.connector.Error as error:
+            print("Failed to execute query: {}".format(error))
 
     def pop(self, name):
-        sql = '''INSERT INTO users_ac(id) VALUES (?)'''
-        self.cursor.execute(sql, (name,))
-        return self.connection.commit()
+        try:
+            sql = '''INSERT INTO users_ac(id) VALUES (?)'''
+            self.cursor.execute(sql, (name,))
+            return self.connection.commit()
+        except mysql.connector.Error as error:
+            print("Failed to execute query: {}".format(error))
