09340aa638867dc2a8c4bde8bb53497b4b6c5afc
SagidKamilov <89615854074a@gmail.com>
2023-04-08 01:04:45 +0300

    изменил DB

--- falsk_app/flask_app.py
+++ falsk_app/flask_app.py
@@ -2,19 +2,18 @@ from flask import Flask, request
 import database.handler
 
 app = Flask(__name__)
-db = database.handler.DataBase()
 
 @app.route('/register')
 def register():
     name = request.args.get('name')
     email = request.args.get('email')
     password = request.args.get('password')
-    return db.register(name, email, password)
+    return database.handler.DataBase().register(name, email, password)
 
 @app.route('/login')
 def login():
     email = request.args.get('email')
     password = request.args.get('password')
-    return db.login(email, password)
+    return database.handler.DataBase().login(email, password)
 
 
