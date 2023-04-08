0df5d6472e5d8f31b5b63bae4bc69a5c73afcd1c
SagidKamilov <89615854074a@gmail.com>
2023-04-08 10:42:34 +0300

    убрал user_id

--- falsk_app/flask_app.py
+++ falsk_app/flask_app.py
@@ -5,14 +5,13 @@ app = Flask(__name__)
 
 @app.route('/register', methods=['POST'])
 def register():
-    id_user = request.args.get('id_user')
     login_u = request.args.get('login')
     email = request.args.get('email')
     password = request.args.get('password')
     name = request.args.get('name')
     age = request.args.get('age')
     sex = request.args.get('sex')
-    return database.handler.DataBase().register(id_user=id_user, login=login_u, email=email, password=password, name=name, age=age, sex=sex)
+    return database.handler.DataBase().register(login=login_u, email=email, password=password, name=name, age=age, sex=sex)
 
 @app.route('/login', methods=['GET'])
 def login():
