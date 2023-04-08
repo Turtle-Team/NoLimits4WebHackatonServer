b521a47e349f5efc898dce03c83aad5a25849958
SagidKamilov <89615854074a@gmail.com>
2023-04-08 10:27:42 +0300

    Тест

--- database/handler.py
+++ database/handler.py
@@ -9,12 +9,10 @@ class DataBase:
         self.cursor = self.connection.cursor()
 
     def register(self, id_user, login, email, password, name, age, sex):
-        sql = '''INSERT INTO users(id_user, login, email, password, name, age, sex) VALUES (%s, %s, %s, %s, %s, %s, %s)'''
+        sql = '''INSERT INTO users(id_user, login, email, password, name, age, sex) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id_user, login, email, password, name, age, sex;'''
         self.cursor.execute(sql, (id_user, login, email, password, name, age, sex))
-        if self.connection.commit():
-            return (id_user, login, email, password, name, age, sex)
-        else:
-            return 'Error, not data...'
+        self.connection.commit()
+
 
     def login(self, login, password):
         sql = '''SELECT id_user, login, email, password, name, age, sex FROM users WHERE login=%s AND password=%s'''
