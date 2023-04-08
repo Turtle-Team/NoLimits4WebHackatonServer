3bf2671acb53106ff1c00a62456c60f8d861b64a
SagidKamilov <89615854074a@gmail.com>
2023-04-08 11:32:28 +0300

    test

--- database/handler.py
+++ database/handler.py
@@ -14,7 +14,7 @@ class DataBase:
             sql = '''INSERT INTO users(id_user, login, email, password, name, age, sex) VALUES (?, ?, ?, ?, ?, ?, ?);'''
             self.cursor.execute(sql, (0, login, email, password, name, age, sex))
             self.connection.commit()
-            sql2 = '''SELECT * FROM users WHERE login = %s;'''
+            sql2 = '''SELECT id_user, login, email, password, name, age, sex FROM users WHERE login = %s;'''
             self.cursor.execute(sql2, (login,))
             user_data = self.cursor.fetchone()
             user = {
@@ -34,9 +34,15 @@ class DataBase:
     def login(self, login, password):
         sql = "SELECT id_user, login, email, password, name, age, sex FROM users WHERE login=%s AND password=%s;"
         self.cursor.execute(sql, (login, password))
-        result = self.cursor.fetchone()
-        if result:
-            print(result)
-        else:
-            return 'Error, not data...'
+        user_data = self.cursor.fetchone()
+        user = {
+            'id_user': user_data[0],
+            'login': user_data[1],
+            'email': user_data[2],
+            'password': user_data[3],
+            'name': user_data[4],
+            'age': user_data[5],
+            'sex': user_data[6]
+        }
+        return json.dumps(user)
 
